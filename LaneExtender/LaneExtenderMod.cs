a716c507916d8d2d408cb006dddf26b3f878f6bb
Bram Fokke <bram.fokke@ambacht.it>
2022-09-12 08:58:20 +0200

    Initial mod implementation

---
+++ LaneExtender/LaneExtenderMod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ICities;
+using UnityEngine;
+
+namespace LaneExtender
+{
+    public class LaneExtenderMod : IUserMod
+    {
+        public string Name => "Lane Extender";
+
+        public string Description => "Allows you to extend freeway lanes easily without messing up alignment of the roads.";
+    }
+}
